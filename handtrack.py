18769834324a4140eba831a228a8f39dbbc09c22
Friedrich.xc <163061094+friedcode4@users.noreply.github.com>
2025-02-09 18:04:38 +0100

    Update handtrack.py

--- handtrack.py
+++ handtrack.py
@@ -17,10 +17,10 @@ success, img = cap.read()
  imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
  results = hands.process(imgRGB)
 
-for id, lm in enumerate(handLms.landmark): h,w,c = img.shape
-cx,cy = int(lm.x*w), int(lm.y*h)
-print( id, cx, cy)
-if id==0:
+ for id, lm in enumerate(handLms.landmark): h,w,c = img.shape
+ cx,cy = int(lm.x*w), int(lm.y*h)
+ print( id, cx, cy)
+  if id==0:
   cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
 
 mpDraw.draw_landmarks(img, handLms, mphands.HAND_CONNECTIONS)
