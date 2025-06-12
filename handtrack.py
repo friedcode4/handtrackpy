c97c9b10c7b600dd62e383cb456c863361eb8512
Friedrich.xc <163061094+friedcode4@users.noreply.github.com>
2025-06-12 21:22:36 +0200

    Update handtrack.py

--- handtrack.py
+++ handtrack.py
@@ -12,29 +12,29 @@ pTime= 0
 cTime= 0
 
 while True:
-success, img = cap.read()
+           success, img = cap.read()
            
- imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
- results = hands.process(imgRGB)
+            imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            results = hands.process(imgRGB)
 
  for id, lm in enumerate(handLms.landmark): h,w,c = img.shape
- cx,cy = int(lm.x*w), int(lm.y*h)
- print( id, cx, cy)
+            cx,cy = int(lm.x*w), int(lm.y*h)
+            print( id, cx, cy)
   if id==0:
-  cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
+             cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
 
-mpDraw.draw_landmarks(img, handLms, mphands.HAND_CONNECTIONS)
+           mpDraw.draw_landmarks(img, handLms, mphands.HAND_CONNECTIONS)
             
-cTime = time.time()
-fps = 1/(cTime-pTime)
-pTime = cTime
+           cTime = time.time()
+           fps = 1/(cTime-pTime)
+           pTime = cTime
             
-cv2.putText( img, str(int(fps)),(10,70), cv2.FONT_HERSEY_PLAIN, 3, (255,0,255),3)
-cv2.imshow("image", img)
+           cv2.putText( img, str(int(fps)),(10,70), cv2.FONT_HERSEY_PLAIN, 3, (255,0,255),3)
+           cv2.imshow("image", img)
 if cv2.waitkey(1) & 0*FF == ord('q'):
                 
- cap.release()
-cv2.destroyAllWindows()
+            cap.release()
+           cv2.destroyAllWindows()
                      
         //follow me on instagram: @im_friedrich    
             
