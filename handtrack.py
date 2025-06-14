13312d9f63ea50f9c23df0bd8369ae838526c013
Friedrich Dikongue <163061094+friedcode4@users.noreply.github.com>
2025-06-14 13:34:37 +0200

    handtrack.py
    Code python permettant le suivi de la main pouvant intervenir dans plusieurs projets comme la détection de mouvement ou le contrôle manuelle sans contact. Modifier certaines variables et conditions en fonction de votre usage souhaité.

--- handtrack.py
+++ handtrack.py
@@ -2,29 +2,28 @@ import cv2
 import mediapipe as mp
 import time
 
-cap= cv2.Videocapture(0) 
+cap= cv2.VideoCapture(0) 
 
 mphands = mp.solutions.hands
 hands = mphands.Hands(False)
-mpDraw = mp.solutions. drawing_utils
+mpDraw = mp.solutions.drawing_utils
 
 pTime= 0
 cTime= 0
 
 while True:
-           success, img = cap.read()
-            imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-            results = hands.process(imgRGB)
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
 
  for id, lm in enumerate(handLms.landmark): 
-            h,w,c = img.shape
-            cx,cy = int(lm.x*w), int(lm.y*h)
-            print( id, cx, cy)
+    h,w,c = img.shape
+    cx,cy = int(lm.x*w), int(lm.y*h)
+    print( id, cx, cy)
             
-  if id==0:
-             cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
-
-            mpDraw.draw_landmarks(img, handLms, mphands.HAND_CONNECTIONS)
+if id==0:
+     cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
+     mpDraw.draw_landmarks(img, handLms, mphands.HAND_CONNECTIONS)
             
            cTime = time.time()
            fps = 1/(cTime-pTime)
@@ -32,7 +31,7 @@ while True:
             
            cv2.putText( img, str(int(fps)),(10,70), cv2.FONT_HERSEY_PLAIN, 3, (255,0,255),3)
            cv2.imshow("image", img)
-if cv2.waitkey(1) & 0*FF == ord('q'):
+if cv2.waitKey(1) & 0xFF == ord('q'):
                 
             cap.release()
            cv2.destroyAllWindows()
